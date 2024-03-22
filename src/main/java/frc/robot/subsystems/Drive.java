ab2769409de8a8cac2910514686421a0684474d2
sadhalan <sadhaleanisha@gmail.com>
2024-03-21 20:23:47 -0400

    Ability to invert motors (if needed)

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -5,7 +5,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -25,6 +24,10 @@ public class Drive extends SubsystemBase {
 
     diffDrive = new DifferentialDrive(left1, right1);
 
+    //If you find that one side of motors is going in the wrong direction, you can uncomment one or both of the lines below to invert your motors
+
+    // left1.setInverted(true);
+    // right1.setInverted(true);
   }
 
   public void drive(double speed, double rotation) {
